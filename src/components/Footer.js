eac038974a0712dee3cc70f1e9d0df1b622a3845
Matheus Pugliese <matheus.pugliese8@gmail.com>
2022-03-18 12:15:36 -0300

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Button } from './Button';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adventure newsletter for receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time
+        </p>
+        <div className="input-area">
+          <form>
+            <input type="email" name="email" placeholder="Your Email"className="footer-input"/>
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className="social-media-wrap">
+          <div className='footer-logo'>
+            <Link to='/' className='social-logo'>
+              TRVL <i className='fab fa-typo3'></i>
+            </Link>
+          </div>
+          <small className='website-rights'>TRVL</small>
+          <div className='social-icons'>
+            <Link className='social-icon-link facebook' to='/' target='_blank' aria-label='Facebook' >
+              <i className='fab fa-facebook-f'></i>
+            </Link>
+            <Link className='social-icon-link twitter' to='/' target='_blank' aria-label='Twitter' >
+              <i className='fab fa-twitter'></i>
+            </Link>
+            <Link className='social-icon-link instagram' to='/' target='_blank' aria-label='Instagram' >
+              <i className='fab fa-instagram'></i>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
